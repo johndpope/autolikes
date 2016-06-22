4a0def9aa8c03981e648b6360aa9155fcdd1cc44
Juan Pujol <contact@juanpujol.com>
2016-06-22 00:10:57 -0300

    refactor: improve docs on client.js

--- common/models/client.js
+++ common/models/client.js
@@ -1,5 +1,13 @@
 module.exports = function(Client) {
 
+  /**
+   * Disable methods that are not necessary.
+   */
+  Client.disableRemoteMethod('createChangeStream', true);
+
+  /**
+   * Update the property updatedAt every time the model is saved
+   */
   Client.beforeRemote('prototype.updateAttributes', function(ctx, client, next) {
     var body = ctx.req.body;
     body.updatedAt = new Date();
